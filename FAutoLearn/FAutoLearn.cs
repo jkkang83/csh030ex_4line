44a8095115c815d7822ffcc13cf6b8cd229e8677
황정욱 <whangj88@gmail.com>
2026-09-21 15:47:07 +0900

    1. FautoLearn 수정 (26092103)

--- FAutoLearn/FAutoLearn.cs
+++ FAutoLearn/FAutoLearn.cs
@@ -10979,6 +10979,12 @@ namespace FAutoLearn
             if (si == 0)
                 Xoffset = new int[3] { 1, 0, -1 };    //  side N 마크는 우측부터 검색
 
+            int valleyThreshold = 8;
+            if (si == 2)
+            {
+                valleyThreshold = 13;
+            }
+
             //else if (si == 4)
             //    Xoffset = new int[3] { 1, 0, -1 };    //  side N 마크는 우측부터 검색
 
@@ -11081,7 +11087,7 @@ namespace FAutoLearn
                         //    afterValley = true;
                         if (aVline_i_k < 100)
                         {
-                            if (aVline_i_k < lastPeak - 12 && aVline[i][k + 1] >= aVline_i_k && aVline[i][k - 1] >= aVline_i_k)    //  어떤 점이 직전 Peak 보다 20 이상 어두운데 다음 점이 밝은 경우
+                            if (aVline_i_k < lastPeak - valleyThreshold && aVline[i][k + 1] >= aVline_i_k && aVline[i][k - 1] >= aVline_i_k)    //  어떤 점이 직전 Peak 보다 20 이상 어두운데 다음 점이 밝은 경우
                                 if (aVline_i_k < minPeak) //  Valley 는 minPeak 보다 어두워야 한다.
                                 {
                                     lastValley = aVline_i_k;
